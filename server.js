c7f8c9a57322fe704985cd9a1e93f00c56ed4b64
aj217 <jani.ayush217@gmail.com>
2024-11-13 02:21:58 +0000

    In server.js static file configuration should allow user to access files in public directly.

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ connectDB();
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 // Serve static files from the 'images' folder
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
+
 
 app.use(cors());
 
